41e74bcdfc582df11130273f76d0b0e035166908
Sadia Reza <sadia.reza@northsouth.edu>
2026-05-02 02:25:05 +0600

    fix: move GIF to res/raw and update CpuAnimation to use R.raw

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("io.coil-kt:coil-gif:2.6.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
 }
